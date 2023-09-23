285f5617806352fa66c49e49e09e26b6eedb9475
Ramdas Gupta <er.rdgupta@gmail.com>
2023-09-23 19:37:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import the HTTP module
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+// Listen on port 3000
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
